15e5bd78630400908e1b9981cd7b73ba72ca0746
cjoanne - Oleg Vastaev <cjoanne@student.21-school.ru>
2021-11-28 12:20:48 +0300

    right textures place

--- sources/game_algs/game_start.c
+++ sources/game_algs/game_start.c
@@ -106,9 +106,9 @@ void	start_game(t_game *game)
 	game->texs = malloc(4 * sizeof(t_texture));
 	game_init(game);
 	init_texture(game, &game->texs[0], game->map->format->EA);
-	init_texture(game, &game->texs[1], game->map->format->NO);
-	init_texture(game, &game->texs[2], game->map->format->SO);
-	init_texture(game, &game->texs[3], game->map->format->WE);
+	init_texture(game, &game->texs[1], game->map->format->WE);
+	init_texture(game, &game->texs[2], game->map->format->NO);
+	init_texture(game, &game->texs[3], game->map->format->SO);
 	game->map->m[game->map->player->y][game->map->player->x] = '0';
 	mlx_hook(game->window, 17, 0, exit_game, game);
 	mlx_hook(game->window, 2, 0, key_press, game);
